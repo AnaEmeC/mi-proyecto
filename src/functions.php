b34183978a1c1100deec307648d66cb36ef72ece
AnaMoya <anmoco01@alumnatflorida.es>
2024-09-20 17:59:57 +0200

    Séptimo commit: añado la nueva función personalizada a functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,14 @@
 <?php
 function saludo()
 {
-    return '¡Hola desde functions.php!';
+    return "¡Hola desde functions.php!";
 }
 echo saludo();
+
+function saludo2($nombre)
+{
+    return "¡Hola desde $nombre!";
+}
+
+$nombre1 = "BobEsponja";
+echo saludo2($nombre1);
